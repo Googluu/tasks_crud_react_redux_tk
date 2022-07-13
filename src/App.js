fc5d882bd9077c4e2294746d81b329ca1198916c
GlooGluu <sneyder.h.rodriguez@gmail.com>
2022-07-13 17:36:23 -0500

    :speech_balloon: add tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import { useSelector } from 'react-redux'
 import TaskForm from './components/TaskForm';
 import TasksList from './components/TasksList';
 
 function App() {
-  const tasksState = useSelector(state => state.tasks)
-  console.log(tasksState);
 
   return (
     <div className="App">
