e8389d0dbf6c20329ff35f17d3fefb3f9ea118aa
Sneyder <sneyder.h.rodriguez@gmail.com>
2023-03-31 11:20:42 -0500

    inte-save localstorage-reduxth #0001err

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,8 @@
-import {configureStore} from '@reduxjs/toolkit';
-import tasksReducer from '../features/tasks/taskSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import tasksReducer from "../features/tasks/taskSlice";
 
 export const store = configureStore({
-    reducer: {
-        tasks: tasksReducer
-    },
+  reducer: {
+    todos: tasksReducer,
+  },
 });
-
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,74 +1,77 @@
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { addTask, editTask } from '../features/tasks/taskSlice';
-import { v4 as uuid } from 'uuid';
-import {useNavigate, useParams} from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/taskSlice";
+import { v4 as uuid } from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
 
 function TaskForm() {
-
   const [task, setTask] = useState({
     title: "",
-    description: ""
-  })
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const params = useParams()
-  const tasks = useSelector(state => state.tasks)
+    description: "",
+  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+  const tasks = useSelector((state) => state.tasks);
 
   const handleChange = (e) => {
     setTask({
       ...task,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(params.id) {
-      dispatch(editTask(task))
+    if (params.id) {
+      dispatch(editTask(task));
     } else {
-      
-    dispatch(addTask({
-      ...task,
-      id: uuid(),
-     })
-    )
-  }
-    navigate('/')
-  }
+      dispatch(
+        addTask({
+          ...task,
+          id: uuid(),
+        })
+      );
+    }
+    navigate("/");
+  };
 
   useEffect(() => {
-    if(params.id) {
-      setTask(tasks.find(task => task.id === params.id));
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
     }
-  }, [params.id, tasks])
+    localStorage.setItem("tasks", JSON.stringify(task));
+  }, [params.id, tasks, task]);
 
-    return (
-      <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4 mb-2">
-        <label htmlFor="title" className="block text-sm font-bold">Task:</label>
-        <input 
-        name="title" 
-        type="text" 
-        placeholder="title" 
+  return (
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4 mb-2">
+      <label htmlFor="title" className="block text-sm font-bold">
+        Task:
+      </label>
+      <input
+        name="title"
+        type="text"
+        placeholder="title"
         onChange={handleChange}
-        value={task.title} 
+        value={task.title}
         className="w-full p-2 rounded-md bg-zinc-600 mb-2"
-        />
+      />
 
-        <label htmlFor="description" className="block text-xs font-bold mb-2">Description:</label>
-        <textarea 
-        name="description" 
-        placeholder="description" 
+      <label htmlFor="description" className="block text-xs font-bold mb-2">
+        Description:
+      </label>
+      <textarea
+        name="description"
+        placeholder="description"
         onChange={handleChange}
-        value={task.description} 
+        value={task.description}
         className="w-full p-2 rounded-md bg-zinc-600 mb-2"
-        ></textarea>
+      ></textarea>
 
-        <button className="bg-indigo-600 px-2 py-1">Save</button>
-      </form>
-    )
-  }
-  
-  export default TaskForm;
-  
+      <button className="bg-indigo-600 px-2 py-1">Save</button>
+    </form>
+  );
+}
+
+export default TaskForm;
